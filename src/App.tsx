7b5b2ea26faf44740ba3495a5d47a076e733b2bc
MarcusSinclair <31100537+MarcusSinclair@users.noreply.github.com>
2022-03-04 11:43:20 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Marcus Sinclair</h1>
+      <p>Currently under construction...</p>
+    </div>
+  );
+}
